05132b2e58904ff62af0ceeb7a6a8c7e196bf6ef
john-mirage <chaosdynamix@posteo.net>
2022-03-24 20:23:55 +0100

    fix: fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('vite')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/nft-preview-card/",
+    base: "/nft-preview-card-component/",
 })
